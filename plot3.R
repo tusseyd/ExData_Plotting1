8a3f2272ecf217c76eea796ba0ffc9268b844f00
David Tussey <davidtussey@gmail.com>
2014-10-12 20:00:32 -0400

    Try #2

--- plot3.R
+++ plot3.R
@@ -24,5 +24,5 @@ points(power.data.datetime,power.data$Sub_metering_2, col="red", type="l")
 points(power.data.datetime,power.data$Sub_metering_3, col="blue", type="l")
 legend("topright", 
        legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
-       col=c("black", "red", "blue"), pch="-")
+       col=c("black", "red", "blue"), pch="_")
 dev.off()
